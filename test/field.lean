493002625cf0469349b04066e31f747788618e4f
Paul-Nicolas Madelaine <paul-nicolas.madelaine@ens.fr>
2019-07-08 20:26:46 +0200

    tests to showcase gain in speed

--- test/field.lean
+++ test/field.lean
@@ -2,73 +2,26 @@ import tactic.field
 
 open field tactic
 
-def i : num := 0
-def j : num := 1
-def t1 : @nterm ℚ _ := i * (1 / 10 : ℚ) + i * (1 / 10 : ℚ) - i * (1 / 5 : ℚ)
-def t2 : @nterm ℚ _ := i - i
+def f (n : ℕ) : ℕ → @nterm ℚ _
+| 0     := nterm.atom n * 1
+| (i+1) := f i + nterm.atom (n-i-1) * 1
+--f produces a normalized term
 
---lemma slow : t1.norm = 0 := rfl
-lemma fast : t2.norm = 0 := rfl
+def g (n : ℕ) : ℕ → @nterm ℚ _
+| 0     := nterm.atom 0 * 1
+| (i+1) := (nterm.atom (i+1) * 1) + (g i)
+--g produces a term that should normalize to f
 
-namespace hidden
 
-constants (x y : ℝ) (hx : x ≠ 0) (hy : y = 0)
-
-theorem test : x * x⁻¹ * y = 0 :=
-begin
-  field1,
-  { rw hy, norm_cast },
-  { apply hx }
-end
-
-end hidden
-
-namespace pmerge
-
-def foo (n : ℕ) : ℕ → @nterm ℚ _
-| 0     := nterm.atom n
-| (i+1) := foo i * nterm.atom (n-i-1)
-
-def bar (n : ℕ) : ℕ → @nterm ℚ _
-| 0     := nterm.atom 0
-| (i+1) := nterm.atom (i+1) * bar i
-
-def n : ℕ := 15
+def h (n : ℕ) : ℕ → @nterm ℚ _
+| 0     := nterm.atom 0 * 1
+| (i+1) := nterm.norm_sum (nterm.atom (i+1) * 1) (h i)
+--h builds a term following the same pattern as g,
+--but using norm_sum which adds terms while maintaining the normalized form
 
 set_option profiler true
-theorem test_1 : (foo n n).norm = foo n n := rfl
-theorem test_2 : (bar n n).norm = foo n n := rfl
 
-end pmerge
+def n : ℕ := 10
+theorem fast : (h n n) = (f n n)  := rfl
+theorem slow : (g n n).norm = (f n n) * 1 := rfl
 
-namespace smerge
-
-def foo (n : ℕ) : ℕ → @nterm ℚ _
-| 0     := nterm.atom n
-| (i+1) := foo i + nterm.atom (n-i-1)
-
-def bar (n : ℕ) : ℕ → @nterm ℚ _
-| 0     := nterm.atom 0
-| (i+1) := nterm.atom (i+1) + bar i
-
-def n : ℕ := 15
-
-set_option profiler true
-theorem test_1 : (foo n n).norm = foo n n := rfl
-theorem test_2 : (bar n n).norm = foo n n := rfl
-
-end smerge
-
-def foo : ℕ → @nterm ℚ _
-| 0     := i
-| (n+1) := foo n * foo n
-
-def bar : ℕ → @nterm ℚ _
-| 0     := i
-| (n+1) := bar n + bar n
-
-set_option profiler true
-theorem test_mul : (foo 6).norm = i ^ 64 := rfl
-theorem test_add : (bar 6).norm = i * (64 : ℚ) := rfl
-
-theorem test_numerals : (8 : @nterm ℚ _).norm = ((8 : ℚ) : @nterm ℚ _) := rfl
